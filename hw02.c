32242a6157321c27a3cf1d05c43b5db5d0940fa0
Özkan Özlü <ozkanozlu@hotmail.com>
2023-09-18 23:05:17 +0300

    Update hw02.c

--- hw02.c
+++ hw02.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+ #include <stdio.h>
 
 // Constants
-#define USD_TO_CAD 1.35
-#define GALLON_TO_LITERS 3.785
-#define GALLON_TO_BARREL 31.5
+#define USD_TO_CAD 1.35  //1 USD = 1.35 CAD
+#define GALLON_TO_LITERS 3.785 //1 gallon = 3.785 liters
+#define GALLON_TO_BARREL 31.5  // 1 barrel = 31.5 gallons
 
 int main()
 {
@@ -23,16 +23,14 @@ int main()
     scanf("%lf", &startingCurrency);
     printf("Enter starting unit (1-g, 2-l, 3-b) -> ");
     scanf("%lf", &startingUnit);
-
-    // Display the appropriate prompt based on input values for currency and unit
     printf("Enter current price per %c (%s) -> ",
-           'g' + ('l' - 'g') * (startingUnit - 1) * (startingUnit - 2) / 2 + ('b' - 'g') * (startingUnit - 1) * (startingUnit - 2) / 2,
-           "USD" + 3 * (startingCurrency - 1));
+           (startingUnit == 1 ? 'g' : (startingUnit == 2 ? 'l' : 'b')),
+           (startingCurrency == 1 ? "USD" : "CAD"));
     scanf("%lf", &price);
 
     // Convert to USD and gallons
-    pricePerGallonUSD = price / (1.0 + (GALLON_TO_LITERS - 1) * (startingUnit - 1) * (startingUnit - 2) / 2 + (GALLON_TO_BARREL - 1) * (startingUnit - 1) * (startingUnit - 2) / 2);
-    pricePerGallonUSD /= (1.0 + (USD_TO_CAD - 1) * (startingCurrency - 1));
+    pricePerGallonUSD = price / (startingUnit == 1 ? 1 : (startingUnit == 2 ? GALLON_TO_LITERS : GALLON_TO_BARREL));
+    pricePerGallonUSD /= (startingCurrency == 1 ? 1 : USD_TO_CAD);
 
     // Calculate other units and currencies
     pricePerGallonCAD = pricePerGallonUSD * USD_TO_CAD;
@@ -41,15 +39,13 @@ int main()
     pricePerBarrelUSD = pricePerGallonUSD * GALLON_TO_BARREL;
     pricePerBarrelCAD = pricePerGallonCAD * GALLON_TO_BARREL;
 
-    // Output
+    // Formatted Output 
     printf("\n");
     printf("--- Unit --- USD ----- CAD ---\n");
     printf("Gallon:  $  %6.2lf   $  %6.2lf\n", pricePerGallonUSD, pricePerGallonCAD);
     printf("Liter:   $  %6.2lf   $  %6.2lf\n", pricePerLiterUSD, pricePerLiterCAD);
     printf("Barrel:  $  %6.2lf   $  %6.2lf\n", pricePerBarrelUSD, pricePerBarrelCAD);
     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-
-
 
     return 0;
 }
